4034a099e164ea1aa0407415e0a47e9f85d01320
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-09-20 21:49:13 +0200

    Create harbour-networkinfo-sv.ts
    Swedish translation if wanted!

---
+++ translations/harbour-networkinfo-sv.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <source>About Network Info</source>
+        <translation>Om Network Info</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;br&gt;This program is distributed in the hope that it will be useful, but &lt;strong&gt;without any warranty&lt;/strong&gt;; without even the implied warranty of &lt;strong&gt;merchantability&lt;/strong&gt; or &lt;strong&gt;fitness for a particular purpose&lt;/strong&gt;.</source>
+        <translation>Detta program är fri mjukvara! Du kan återdistribuera det och/eller modifiera det, under villkoren för GNU General Public License som den publiceras av Free Software Foundation. Antingen version 3 av licensen, eller valfri senare version.&lt;br&gt;Programmet distribueras i hopp om att det skall vara användbart, &lt;/strong&gt;men &lt;/strong&gt;utan några garantier&lt;/strong&gt;. Utan ens underförstådd garanti om &lt;strong&gt;säljbarhet&lt;/strong&gt; eller &lt;strong&gt;lämplighet för ett visst ändamål&lt;/strong&gt;.</translation>
+    </message>
+    <message>
+        <source>See the GNU General Public License for more details.&lt;/font&gt;&lt;/a&gt;</source>
+        <translation>Se GNU General Public License för mer detaljerad info.&lt;/font&gt;&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>This app provides some information of the local networks that your Jolla Phone is connected to. There are also some useful networking tools like &lt;i&gt;ping&lt;/i&gt; included.</source>
+        <translation>Denna app tillhandahåller viss information om det lokala nätverk som din Sailfish-enhet är ansluten till. Denna innehåller också några användbara nätverksverktyg, som &lt;i&gt;Ping&lt;/i&gt;.</translation>
+    </message>
+</context>
+<context>
+    <name>CommandOutput</name>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+</context>
+<context>
+    <name>CommandUpdate</name>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+    <message>
+        <source>IP address or Hostname</source>
+        <translation>IP-adress eller värdnamn</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Interfaces</source>
+        <translation>Gränssnitt</translation>
+    </message>
+</context>
+<context>
+    <name>IfConfigPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Network Settings</source>
+        <translation>Nätverksinställningar</translation>
+    </message>
+    <message>
+        <source>Interfaces</source>
+        <translation>Gränssnitt</translation>
+    </message>
+    <message>
+        <source>Mac Address</source>
+        <translation>Mac-adress</translation>
+    </message>
+    <message>
+        <source>Netmask</source>
+        <translation>Nätmask</translation>
+    </message>
+    <message>
+        <source>About …</source>
+        <translation>Om ...</translation>
+    </message>
+    <message>
+        <source>Show ifconfig Details</source>
+        <translation>Visa ifconfig-info</translation>
+    </message>
+    <message>
+        <source>Network Tools</source>
+        <translation>Nätverksverktyg</translation>
+    </message>
+    <message>
+        <source>Host</source>
+        <translation>Värd</translation>
+    </message>
+    <message>
+        <source>Domain</source>
+        <translation>Domän</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation>Kapslad sida</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation>Objekt</translation>
+    </message>
+</context>
+<context>
+    <name>ToolsPage</name>
+    <message>
+        <source>Network Tools</source>
+        <translation>Nätverksverktyg</translation>
+    </message>
+    <message>
+        <source>number of pings</source>
+        <translation>Antal ping</translation>
+    </message>
+    <message>
+        <source>hops</source>
+        <translation>hopp</translation>
+    </message>
+</context>
+<context>
+    <name>URLCommand</name>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+    <message>
+        <source>Compress</source>
+        <translation>Komprimera</translation>
+    </message>
+    <message>
+        <source>… HTTP transfer</source>
+        <translation>… HTTP-överföring</translation>
+    </message>
+</context>
+<context>
+    <name>Whois</name>
+    <message>
+        <source>Copy IPv6 to Clipboard</source>
+        <translation>Kopiera IPv6 till urklipp</translation>
+    </message>
+    <message>
+        <source>Copy IPv4 to Clipboard</source>
+        <translation>Kopiera IPv4 till urklipp</translation>
+    </message>
+    <message>
+        <source>Hostname</source>
+        <translation>Värdnamn</translation>
+    </message>
+</context>
+</TS>
